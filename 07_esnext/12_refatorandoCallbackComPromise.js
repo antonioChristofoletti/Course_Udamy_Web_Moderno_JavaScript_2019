725d9747e5e35b6bfe2c96182d5d187801428374
Antonio <antoniolucas123@hotmail.com>
2019-05-08 21:51:24 -0300

    Estudos do Dia 08/05/2019

---
+++ 07_esnext/12_refatorandoCallbackComPromise.js
@@ -0,0 +1,37 @@
+// sem promises
+
+const http = require('http');
+
+const getTurma = (letra, callback) => {
+    return new Promise((resolve, reject) => {
+        const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`;
+        http.get(url, res => {
+            let resultado = '';
+
+            res.on('data', dados => {
+                resultado += dados;
+            });
+
+            res.on('end', dados => {
+                resolve(JSON.parse(resultado));
+            });
+        });
+    });
+};
+
+let nomes = [];
+
+getTurma('A', )
+
+getTurma('A', alunos => {
+    nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`));
+
+    getTurma('B', alunos => {
+        nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`));
+
+        getTurma('C', alunos => {
+            nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`));
+            console.log(nomes);
+        });
+    });
+});
